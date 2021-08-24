d718cc1264f65378047935ca8354370d5a4fc5ea
Koo-YuKyung <grayzone13@nate.com>
2021-08-25 01:57:30 +0900

    vue3-webpack

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 @click="increase">
+    {{ count }} 
+  </h1>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    let count = ref(0)
+    function increase() {
+      count.value += 1
+    }
+
+    return {
+      count,
+      increase
+      }
+    }
+  }
+
+</script>
---
+++ src/components/Parent.vue
@@ -0,0 +1,32 @@
+<template>
+  <Child />
+</template>
+
+<script>
+import Child from '~/components/Child'
+
+export default {
+  components: {
+    Child
+  }
+}
+</script>
+
+
+
+<style scoped lang="scss">
+.btn {
+  display: inline-block;
+  margin: 4px;
+  padding: 6px 12px;
+  border-radius: 4px;
+  background-color: darkmagenta;
+  color: white;
+  cursor: pointer;
+  &.large {
+  font-size: 20px;
+  padding: 10px 20px;
+  }
+}
+
+</style>
